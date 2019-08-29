e3e5a024b5a5bb26d3e9f22cc3e3bb5d0b647430
owenstegall <owenstegall@owens-air.wireless-1x.unc.edu>
2019-08-29 15:27:07 -0400

    why is that false

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -68,6 +68,10 @@ public class A1Adept {
 				
 				for (int k = 0; k < itemCount; k++) {
 					
+					System.out.println(itemNameArray[k]);
+					
+					System.out.println(nameOfItemBought);
+					
 					System.out.println(itemNameArray[k] == nameOfItemBought);
 					
 					if (itemNameArray[k] == nameOfItemBought) {
