7493f16a72dcbbb0babf97717d199e8abaf0a353
owenstegall <owenstegall@owens-air.wireless-1x.unc.edu>
2019-08-29 14:34:52 -0400

    s

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -122,7 +122,7 @@ public class A1Adept {
 		
 		System.out.println(quantity);
 		
-		System.out.println(itemNameArray);
+		System.out.println(itemNameArray[0]);
 		
 		System.out.println("Biggest: " + biggestName + " (" + maxResult + ")");
 		
