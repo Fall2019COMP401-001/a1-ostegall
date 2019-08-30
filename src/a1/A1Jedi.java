bc2dc0ce11dea419a2970da5234a85e85be894a8
owenstegall <owenstegall@owens-air.wireless-1x.unc.edu>
2019-08-30 12:20:55 -0400

    Changed array initialization

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -32,7 +32,7 @@ public class A1Jedi {
 		
 		//fix array length
 		
-		int[] customerCounterArray = new int[customerCount];
+		int[] customerCounterArray = new int[itemCount];
 		
 		for (int i = 0; i < customerCount; i++) {
 			
